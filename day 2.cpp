27bb4670821780a059602e1dec39fddd74e18b70
pujiana <pujiana353@gmail.com>
2025-06-28 12:20:27 +0700

    Add files via upload

---
+++ day 2.cpp
@@ -0,0 +1,15 @@
+#include<conio.h>
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+garis()
+{ printf("\n----------------------\n");
+}
+
+main()
+{ 
+garis(); 
+cout<<"AMIK BSI - Pondok Labu"<<endl;;
+garis();
+return 0;
+}
