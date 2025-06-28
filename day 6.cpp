434193bf8de01877d6f8a731055d15f303e11c7b
pujiana <pujiana353@gmail.com>
2025-06-29 01:21:33 +0700

    Add files via upload

---
+++ day 6.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i = 1;
+	
+	while (i <= 10){
+		
+		if(i == 5){
+			break;
+		}
+
+cout << i << "+" << i << "=" << (i+i) << endl;
+
+
+i++;
+	}
+	
+	return 0;
+}
